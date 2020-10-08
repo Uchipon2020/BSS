326f769b7a2ef329b4a65699c983cfe0d73f8d2b
uchiyama-g <uchiyama.s@timeshappy.com>
2020-10-08 09:40:32 +0900

    10/08 会社PCより、リスト削除部分について、修正

--- app/src/main/java/com/timeshappy/scoresheet/MainActivity.java
+++ app/src/main/java/com/timeshappy/scoresheet/MainActivity.java
@@ -18,12 +18,11 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
-
+    //初期設定エリア
     Button btn_restart,btn_stop,btn_goal,btn_reset;
     TextView et_title_view;
     String et_teama,et_teamb,title_view,taa,tbb;
     ListView lv_item;
-
 
     private Timer timer;
     private CountUpTimerTask timerTask;
@@ -35,23 +34,19 @@ public class MainActivity extends AppCompatActivity {
 
     ArrayAdapter<String> itemArrayAdapter;
     DatabaseHelper databaseHelper ;
-
+    //コンテキスト
     Item item = new Item(-1,null,0,null,0,0,0,null,null,0,0,null);
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
+        //タイマー初期設定
         delay = 0;
         period = 100;
         count = 4000;
-        // "00:00.0"
+        // "05:00.0"
         zero = getString(R.string.zero);
-
         timerText = findViewById(R.id.countView);
         timerText.setText(zero);
 
@@ -81,34 +76,20 @@ public class MainActivity extends AppCompatActivity {
         et_title_view = findViewById(R.id.title_view);
         et_title_view.setText(title_view);
 
-
-
         // タイマー開始--- タイマーが走っている最中にボタンをタップされたケース
         if (null != timer) {
             timer.cancel();
             timer = null;
         }
-
-        // Timer インスタンスを生成
+        // Timerエリア
         timer = new Timer();
-
-
-        // TimerTask インスタンスを生成
         timerTask = new CountUpTimerTask();
-
-        // スケジュールを設定 100msec // public void schedule (TimerTask task, long delay, long period)
-
+        // public void schedule (TimerTask task, long delay, long period)
         timer.schedule(timerTask, delay, period);
 
-
         /*Stop---------------------------------------------------------------------------------
-        *
-        *
         * 停止作業
-        *
-        * -------------------------------------------------------------------------------------
-        * */
-
+       */
         btn_stop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -125,41 +106,25 @@ public class MainActivity extends AppCompatActivity {
 
                     ShowItemOnListView(databaseHelper);//一覧表示用
                 }
-
             }
-
-
-
         });
-
-
-
         //リスタートボタン-----------------------------------------------------------------------
         btn_restart.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View v) {
-
                 // タイマーが走っている最中にボタンをタップされたケース
                 if (null != timer) {
                     timer.cancel();
                     timer = null;
 
-                    // Timer インスタンスを生成
+                    // Timerエリア
                     timer = new Timer();
-
-                    // TimerTask インスタンスを生成
                     timerTask = new CountUpTimerTask();
-
-                    // スケジュールを設定 100msec
                     //public void schedule (TimerTask task, long delay, long period)
                     timer.schedule(timerTask, delay, period);
-
-                    // カウンター
                     count = stopCount;
                     timerText.setText(stopTime);
                     //Toast.makeText(MainActivity.this,"timer"+stopTime,Toast.LENGTH_SHORT).show();
-
 
                     ShowItemOnListView(databaseHelper);//一覧表示用
 
@@ -167,64 +132,48 @@ public class MainActivity extends AppCompatActivity {
                     btn.setText("再　開");
                 }
             }
-
         });
-
-
         //ゴールボタン---------------------------------------------------------
         btn_goal.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-
                 Toast.makeText(MainActivity.this, "on goal", Toast.LENGTH_SHORT).show();
             }
         });
-
-
         //リセットボタン----------------------------------------------------------------
         btn_reset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 timerText = findViewById(R.id.countView);
                 timerText.setText(zero);
                 count = 40000;
             }
         });
 
-
-
         //アイテムを押して削除-----------------------------------------------------------------------
 
         lv_item.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-             @Override
+            @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-                 delete_data((Item) parent.getItemAtPosition(position));
-
-                 boolean success = databaseHelper.deleteOne((Item) parent.getItemAtPosition(position));
-                Toast.makeText(MainActivity.this,"DELETE"+ success,Toast.LENGTH_SHORT).show();
+                Item del_line = (Item) parent.getItemAtPosition(position);
+                databaseHelper.deleteOne(del_line);
+                 //delete_data((Item) parent.getItemAtPosition(position));
+                 //boolean success = databaseHelper.deleteOne((Item) parent.getItemAtPosition(position));
+                //Toast.makeText(MainActivity.this,"DELETE"+ success,Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this,"DELETE"+ del_line,Toast.LENGTH_SHORT).show();
             }
         });
-
-
     }
 
 
-//リストを再読み込みして最新のものを表示させるメソッド
+    //リストを再読み込みして最新のものを表示させるメソッド
    private void ShowItemOnListView(DatabaseHelper databaseHelper) {
         itemArrayAdapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, databaseHelper.getEveryone());
         lv_item.setAdapter(itemArrayAdapter);
     }
-
-
-//カウンター用処理------------------------------------------------------------------------
-
-class CountUpTimerTask extends TimerTask {
-
-    @Override
+    //カウンター用処理------------------------------------------------------------------------
+    class CountUpTimerTask extends TimerTask {
+        @Override
     public void run() {
         // handlerを使って処理をキューイングする
         handler.post(new Runnable() {
@@ -266,14 +215,12 @@ class CountUpTimerTask extends TimerTask {
         boolean success = databaseHelper.addOne(item);//保存作業
 
         Toast.makeText(MainActivity.this,"保存"+success,Toast.LENGTH_SHORT).show();
-
-
     }
-    public void delete_data(Item _item){
+    /*public void delete_data(Item _item){
         DatabaseHelper databaseHelper = new DatabaseHelper(MainActivity.this);
         boolean success = databaseHelper.deleteOne(_item);
         Toast.makeText(MainActivity.this,"削除"+success,Toast.LENGTH_SHORT).show();
 
-    }
+    }*/
 
 }
